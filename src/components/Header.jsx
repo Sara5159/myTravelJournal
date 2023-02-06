9c3e01a9e17d1e2037476ef6858210db27a88e5d
Sara5159 <sniggid.aras.com>
2023-02-06 18:14:39 +1030

    v0

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <i className="fa-solid fa-earth-oceania header--icon"></i>
+            <h1 className="header--text">my travel journal.</h1>
+        </header>
+    )
+}
