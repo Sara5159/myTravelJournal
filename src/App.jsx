3c41c0f218f1ed8b1867dcf8bfbdaafb305f1fee
Sara5159 <sniggid.aras.com>
2023-02-10 10:11:57 +1030

    v1 review updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,7 @@ import './style.css'
 
 function App() {
   const destinations = data.map(destination => {
-    return (
-      <Card
-      key={destination.id}
-      {...destination}
-      />
-    )
+    return (<Card key={destination.id} {...destination} />)
   })
 
   return (
